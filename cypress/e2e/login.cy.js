be46829ada20f8f60dd13bb235484508f13af4ef
Anurag Pandey <anuragpandey88699@gmail.com>
2025-07-19 03:07:15 +0530

    login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 /// <reference types = "cypress"/>
-const username = "anuragpandey88699@gmail.com";
-const password = "09129709907aA@";
+
+import testData from "../fixtures/data.json";
 
 describe("login test", () => {
   before(() => {
@@ -29,10 +29,10 @@ describe("login test", () => {
     cy.get(".textinput-cell-input-control", { timeout: 10000 })
       .first()
       .should("be.visible")
-      .type(username);
+      .type(testData.username);
     cy.get("[name='password']", { timeout: 10000 })
       .should("be.visible")
-      .type(password);
+      .type(testData.password);
     cy.get("[name ='submitLogin']", { timeout: 2000 })
       .should("be.visible")
       .click();
